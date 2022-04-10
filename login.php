41bb21386f307f5b37c7bc80577495d2fcfa77b2
Adam Jurewicz <mpowergcm@outlook.com>
2022-04-10 17:04:18 +0200

    Dodanie formularza logowania

--- login.php
+++ login.php
@@ -22,11 +22,31 @@ $pol_db = mysqli_connect($dbhost, $dbusername, $dbuserpassword, $default_dbname)
                 <h1 class="login_h1">MY ERP - logowanie</h1>
             </div>
             <div class="login_main_page">
-                <div class="login_form">
+                <div class="login_main_form">
                     <p class="login_adn">
                         Aby się zalogować wpisz login i hasło użytwkonika.
                     </p>
-
+                    <div class="login_form">
+                        <form method="post" action="function.php">
+                            <table class="login_tbl_form">
+                                <tr>
+                                    <td class="login_tbl_form">Login:</td>
+                                    <td class="login_tbl_form"><input type="text" name="login_uz"></td>
+                                </tr>
+                                <tr>
+                                    <td class="login_tbl_form">Hasło:</td>
+                                    <td class="login_tbl_form"><input type="password" name="haslo_uz"></td>
+                                </tr>
+                                <tr>
+                                    <td class="login_tbl_form" colspan="2">
+                                        <input class="login_reset" type="reset">&nbsp;&nbsp;|&nbsp;
+                                        <input class="login_submit" type="submit" value="Zaloguj">
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>                        
+                    </div>
+                    <div class="login_spacer"></div>
                 </div>
             </div>
             <div class="login_footer">
