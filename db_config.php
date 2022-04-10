0b95cea450be2267bb2ed407e84c62107cbf12dc
Adam Jurewicz <mpowergcm@outlook.com>
2022-04-10 15:51:55 +0200

    Dodanie adnotacji - komentarzy do pliku konf...

--- db_config.php
+++ db_config.php
@@ -1,6 +1,10 @@
 <?php
-$dbhost = '23193.m.tld.pl';
-$dbusername = 'admin23193_my_erp';
-$dbuserpassword = '5PxNyujsPH';
-$default_dbname = 'baza23193_my_erp';
-?>
+// plik konfiguracyjny bazy danych
+
+$dbhost = '23193.m.tld.pl';             //host bazy danych
+$dbusername = 'admin23193_my_erp';      // nazwa uzytkownika bazy danych
+$dbuserpassword = '5PxNyujsPH';         // haslo uzytkownika --//--
+$default_dbname = 'baza23193_my_erp';   // baza danych 
+
+// -------------------------------
+>
