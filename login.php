29e657789f6c97f17ced2c5b99706db4447e49e3
Adam Jurewicz <mpowergcm@outlook.com>
2022-04-10 18:08:46 +0200

    Dodanie stylów dla elementów formularza

--- login.php
+++ login.php
@@ -31,14 +31,14 @@ $pol_db = mysqli_connect($dbhost, $dbusername, $dbuserpassword, $default_dbname)
                             <table class="login_tbl_form">
                                 <tr>
                                     <td class="login_tbl_form">Login:</td>
-                                    <td class="login_tbl_form"><input type="text" name="login_uz"></td>
+                                    <td class="login_tbl_form"><input class="log_form" type="text" name="login_uz"></td>
                                 </tr>
                                 <tr>
                                     <td class="login_tbl_form">Hasło:</td>
-                                    <td class="login_tbl_form"><input type="password" name="haslo_uz"></td>
+                                    <td class="login_tbl_form"><input class="paswd_form" type="password" name="haslo_uz"></td>
                                 </tr>
                                 <tr>
-                                    <td class="login_tbl_form" colspan="2">
+                                    <td class="login_tbl_form_but" colspan="2">
                                         <input class="login_reset" type="reset">&nbsp;&nbsp;|&nbsp;
                                         <input class="login_submit" type="submit" value="Zaloguj">
                                     </td>
