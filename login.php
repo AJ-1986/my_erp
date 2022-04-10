732233c972fbbbae1ebe6a5f9f53736d798156f8
Adam Jurewicz <mpowergcm@outlook.com>
2022-04-10 16:25:43 +0200

    Dodanie nagłówka html do strony

--- login.php
+++ login.php
@@ -5,3 +5,18 @@ include './db_config.php'; // plik konfiguracyjny bazy
 $pol_db = mysqli_connect($dbhost, $dbusername, $dbuserpassword, $default_dbname);
 // --------------------------------
 ?>
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="0">
+        <meta name="autor" content="Adam Jurewicz (proxweb@outlook.com)">
+        <title>MY ERP - logowanie</title>
+        <link rel="stylesheet" href="main_style.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
