179befb1bce1a160c4b1a4a0934b4600fb5b5618
AJ-proxweb <proxweb@outlook.com>
2022-04-10 10:34:01 +0200

    Utworzenie pliku startowego i zaimplementowanie kodu początkowego

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="0">
+        <meta name="autor" content="Adam Jurewicz (proxweb@outlook.com)">
+        <title>MY ERP</title>
+        <link rel="stylesheet" href="main_style.css">
+    </head>
+    <body>
+
+    </body>
+</html>
