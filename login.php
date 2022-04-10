b076e0bb734436b379ae135f19546a97e65a555c
Adam Jurewicz <mpowergcm@outlook.com>
2022-04-10 16:23:27 +0200

    Dodanie połączenia z bazą danych

--- login.php
+++ login.php
@@ -1,2 +1,7 @@
 <?php
+include './db_config.php'; // plik konfiguracyjny bazy
+
+// link do połączenia z bazą danych
+$pol_db = mysqli_connect($dbhost, $dbusername, $dbuserpassword, $default_dbname);
+// --------------------------------
 ?>
