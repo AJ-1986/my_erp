9ced711fd5a79c55fde519cf4169a345944cd448
Adam Jurewicz <mpowergcm@outlook.com>
2022-04-11 18:18:08 +0200

    Poprawienie literówki w adresie linku

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ session_start();
                 <div class="logo">MY_ERP</div>                
                 <div class="right_nav_header">
                     <a class="link_header" href="index.php?auth=1&ustawienia=1">Ustawienia</a>&nbsp;&nbsp;|&nbsp;
-                    <a class="link_header" href="funkcje.php?wyloguj=1">Wyloguj</a>
+                    <a class="link_header" href="function.php?wyloguj=1">Wyloguj</a>
                 </div>                
             </div>            
             <div class="left_site">
