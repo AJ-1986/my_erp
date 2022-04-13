d5a30d246ecdb9d740324cb22008c0909660463c
Adam Jurewicz <96868993+AJ-1986@users.noreply.github.com>
2022-04-13 17:20:16 +0200

    Dodanie przycisku 'Raporty'

--- index.php
+++ index.php
@@ -72,6 +72,7 @@ $pol_db = mysqli_connect($dbhost, $dbusername, $dbuserpassword, $default_dbname)
                     <a href="index.php"><img src="images/faktury_zakupu_button.jpg" alt="Faktury zakupu"></a><br><br>
                     <a href="index.php"><img src="images/kontrahenci_button.jpg" alt="Kontrahenci"></a><br><br>
                     <a href="index.php"><img src="images/magazyn_button.jpg" alt="Magazyn"></a><br><br>
+                    <a href="index.php"><img src="images/raporty_button.jpg" alt="Raporty"></a><br><br>
                 </div>
             </div>
             <div class="right_site">
