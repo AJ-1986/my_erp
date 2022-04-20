63864cbf4893244536000e73d6704a4c7ea23db5
Adam Jurewicz <96868993+AJ-1986@users.noreply.github.com>
2022-04-20 12:27:04 +0200

    Dodanie wstępnego opisu dla strony głównej

--- strona_glowna.php
+++ strona_glowna.php
@@ -2,7 +2,15 @@
 if(!empty($_SESSION['log_id']) AND $_SESSION['log_ok'] == $_SESSION['log_id']) {
 
     if($_GET['auth'] == '1') {
-        echo 'Strona główna';
+        echo '
+                    <h1 class="bazowy">Strona główna</h1>
+                    <p class="bazowy">
+                        Witam na stronie głównej systemu MY_ERP.<br><br>
+                        Jest to prosty system do zarządzania sprzedażą oraz kontrahentami.<br><br>
+                        Na stronie tej uzyskasz informacje statystyczne dotyczące twojego statusu. Odnajdziesz tu informację o nowych dodanych kontrahentach, fakturach sprzedaży i zakupu oraz o aktualnych płatnościach dotyczących wybranego podmiotu, na którym aktualnie pracujesz.<br><br>
+                        Dziękuję za użytkowanie.
+                    </p>
+        ';
     }
 }
 else {
