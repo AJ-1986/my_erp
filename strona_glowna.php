d0955cbe399cf69bd97481ccd02d5a6da5c8a741
Adam Jurewicz <96868993+AJ-1986@users.noreply.github.com>
2022-04-20 12:13:48 +0200

    Zabezpieczenie części strony_glownej.php przed nieautoryzowanym dostępem

--- strona_glowna.php
+++ strona_glowna.php
@@ -1,5 +1,19 @@
 <?php
-if($_GET['auth'] == '1') {
-    echo 'Strona główna';
+if(!empty($_SESSION['log_id']) AND $_SESSION['log_ok'] == $_SESSION['log_id']) {
+
+    if($_GET['auth'] == '1') {
+        echo 'Strona główna';
+    }
+}
+else {
+    echo '
+<html lang="pl-PL">
+<body>
+    <script>
+        document.location="login.php";
+    </script>
+</body>
+</html>
+    ';
 }
 ?>
