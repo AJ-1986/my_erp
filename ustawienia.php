563d9f579732e8d585cb00806b1faa95362773a3
Adam Jurewicz <96868993+AJ-1986@users.noreply.github.com>
2022-04-21 14:53:59 +0200

    Dodanie podstawowego kodu startowego dla ustawienia.php

--- ustawienia.php
+++ ustawienia.php
@@ -1,3 +1,25 @@
 <?php
-echo 'ustawienia';
+if(!empty($_SESSION['log_id']) AND $_SESSION['log_ok'] == $_SESSION['log_id']) {
+
+    if($_GET['auth'] == '1') {
+        echo '
+                    <h1 class="bazowy">Ustawienia</h1>
+                    <p class="bazowy">
+                        Opis tymczasowy
+                    </p>
+        ';
+    }
+}
+else {
+    echo '
+<!DOCTYPE html>
+<html lang="pl-PL">
+<body>
+    <script>
+        document.location="login.php";
+    </script>
+</body>
+</html>
+    ';
+}
 ?>
