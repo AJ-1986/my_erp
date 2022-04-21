05d48201b8a0e057a7bd64e3ed728d66776d2938
Adam Jurewicz <96868993+AJ-1986@users.noreply.github.com>
2022-04-21 16:08:01 +0200

    Dodanie zapytania sql o pobranie danych o podmiocie

--- ustawienia.php
+++ ustawienia.php
@@ -6,8 +6,18 @@ if(!empty($_SESSION['log_id']) AND $_SESSION['log_ok'] == $_SESSION['log_id']) {
                     <h1 class="bazowy">Ustawienia</h1>
                     <p class="bazowy">
                         Na tej stronie dokonasz wszelkich modyfikacji związanych z twoimi informacjami dotyczącymi użytkowników, podmiotów.
-                    </p>
+                    </p>                    
         ';
+        // pobieranie podmiotów z bazy danych plus tworzenie formularza do edycji danych
+        $q = "SELECT nazwa_podmiotu FROM podmioty WHERE id_podmiotu LIKE '$_SESSION[podmiot_id]'";
+        $sql = mysqli_query($pol_db, $q);
+
+        while($query_data = mysqli_fetch_row($sql)) {
+            echo '
+            
+            ';
+        }                                
+        //-----------------------------------
     }
 }
 else {
