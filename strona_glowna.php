a55d04bfa4f7ea7f534cf820fbc0e53fa6f71ee4
Adam Jurewicz <96868993+AJ-1986@users.noreply.github.com>
2022-04-23 22:23:20 +0200

    Aktualizacja treści w stronie głównej

--- strona_glowna.php
+++ strona_glowna.php
@@ -7,7 +7,9 @@ if(!empty($_SESSION['log_id']) AND $_SESSION['log_ok'] == $_SESSION['log_id']) {
                     <p class="bazowy">
                         Witam na stronie głównej systemu MY_ERP.<br><br>
                         Jest to prosty system do zarządzania sprzedażą oraz kontrahentami.<br><br>
-                        Na stronie tej uzyskasz informacje statystyczne dotyczące twojego statusu. Odnajdziesz tu informację o nowych dodanych kontrahentach, fakturach sprzedaży i zakupu oraz o aktualnych płatnościach dotyczących wybranego podmiotu, na którym aktualnie pracujesz.<br><br>
+                        Na stronie tej uzyskasz informacje statystyczne dotyczące twojego statusu. Odnajdziesz tu informację o nowych dodanych kontrahentach,
+                        fakturach sprzedaży i zakupu oraz o aktualnych płatnościach dotyczących wybranego podmiotu, na którym aktualnie pracujesz.<br><br>
+                        Wszystkie funkcje związane z aktualizacją danych Twojego podmiotu jak i dodawaniem nowych użytkowników systemu znajdują się w "Ustawieniach".
                         Dziękuję za użytkowanie.
                     </p>
         ';
