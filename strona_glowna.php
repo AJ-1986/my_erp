8a6e484142ad957bb14b576b31c2703325a94a8e
Adam Jurewicz <96868993+AJ-1986@users.noreply.github.com>
2022-04-23 20:24:29 +0200

    Utworzenie instrukcji SQL odczytującej historię zdarzeń

--- strona_glowna.php
+++ strona_glowna.php
@@ -12,7 +12,15 @@ if(!empty($_SESSION['log_id']) AND $_SESSION['log_ok'] == $_SESSION['log_id']) {
                     </p>
         ';
         // wyświetlanie systemowego dziennika zdarzeń
-        
+        $q = "SELECT system_log.log_id, uzytkownicy.login, system_log.data, system_log.godzina, system_log.zdarzenie 
+                FROM system_log 
+                INNER JOIN uzytkownicy ON system_log.user_id=uzytkownicy.id_uz 
+                ORDER BY system_log.log_id DESC LIMIT 15";
+        $sql = mysqli_query($pol_db, $q);
+
+        while($query_data = mysqli_fetch_row($sql)) {
+      
+        }
         // ------------------------------------------
     }
 }
