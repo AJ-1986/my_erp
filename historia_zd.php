131bb9b59b46ad5bb72b4dcc24e8b4b910b0a9b3
Adam Jurewicz <96868993+AJ-1986@users.noreply.github.com>
2022-04-24 13:23:09 +0200

    Zapisanie kodu początkowego dla historia_zd.php

--- historia_zd.php
+++ historia_zd.php
@@ -1,3 +1,20 @@
 <?php
+if(!empty($_SESSION['log_id']) AND $_SESSION['log_ok'] == $_SESSION['log_id']) {
 
+    if($_GET['auth'] == '1') {
+
+    }
+}
+else {
+    echo '
+<!DOCTYPE html>
+<html lang="pl-PL">
+<body>
+    <script>
+        document.location="login.php";
+    </script>
+</body>
+</html>
+    ';
+}
 ?>
