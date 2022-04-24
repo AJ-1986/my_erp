562f9775048eaa06bd52a67632d99d99a836073e
Adam Jurewicz <96868993+AJ-1986@users.noreply.github.com>
2022-04-24 15:43:22 +0200

    Dodanie treści opisowej dla strony z historią zdarzeń

--- historia_zd.php
+++ historia_zd.php
@@ -2,7 +2,13 @@
 if(!empty($_SESSION['log_id']) AND $_SESSION['log_ok'] == $_SESSION['log_id']) {
 
     if($_GET['auth'] == '1') {
-
+        echo '
+                    <h1 class="bazowy">Dziennik zdarzeń</h1>
+                    <p class="bazowy">
+                        Poniżej zostały wyświetlone wszystkie zarejestrowane 
+                        zdarzenia w systemie. Wyświetlone od najnowszego do najstarszego.
+                    </p>
+        ';
     }
 }
 else {
